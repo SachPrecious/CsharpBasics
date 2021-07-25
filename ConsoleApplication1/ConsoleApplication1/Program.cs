b6109a5daa274331832119ff74c1a0d7117dfbb8
SachPrecious <sachithrabandara49@gmail.com>
2021-07-25 23:31:14 +0530

    #2

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -17,8 +17,15 @@ namespace ConsoleApplication1
             Console.WriteLine("Enter a Number");
             aNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Number is " + number + " A Number Is " + aNumber + " Some Number is " + SomeNumber);
-            
+
+
+            double doubleValue = 44.5;
+            Console.WriteLine("Enter a Number");
+            Double aDoubleNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Double Values " + doubleValue + " " + aDoubleNumber);
             Console.ReadKey();
+
+            
         }
     }
 }
